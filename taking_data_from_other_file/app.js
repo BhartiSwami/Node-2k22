3029f47a6352cf1a008a478e2756d06b4259514c
BhartiSwami <bhartiswamitech@gmail.com>
2022-04-10 04:13:35 +0530

    Taking Data From Other File

---
+++ taking_data_from_other_file/app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    no_of_users:53809653210,
+    no_of_active_users:865321109,
+    revenue:function(){
+        return 7642316790891
+    }
+}
---
+++ taking_data_from_other_file/index.js
@@ -0,0 +1,5 @@
+const appData = require('./app')
+
+console.log(appData)
+console.log(appData.no_of_active_users)
+console.log(appData.revenue())
